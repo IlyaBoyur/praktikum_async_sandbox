5c0bdec96a0da4278710f8be5ae3e184411d3b48
Ilya Boyur <i.boyur@idaproject.com>
2023-03-15 18:55:47 +0300

    Add func and class based alternatives

---
+++ multiprocessing/based_on_class.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+from time import sleep
+
+
+class CustomProcess(Process):
+    def __init__(self, limit):
+        super().__init__()
+        self._limit = limit
+
+    def run(self):
+        for i in range(self._limit):
+            print(f'Из CustomProcess: {i}')
+            sleep(0.5)
+
+
+if __name__ == "__main__":
+    custom = CustomProcess(5)
+    custom.start() 
