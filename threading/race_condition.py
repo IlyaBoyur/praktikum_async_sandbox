1d714861d40b69e688cad111a26801cab6c0ee7b
Ilya Boyur <i.boyur@idaproject.com>
2023-03-31 13:02:34 +0300

    Add examples for common thread programming problems

---
+++ threading/race_condition.py
@@ -0,0 +1,24 @@
+# python 3.9.5
+
+from threading import Thread
+
+# Глобальная переменная, которая будет изменяться из нескольких потоков
+counter = 0
+
+def increment():
+    global counter
+    for _ in range(2_000_000):
+        counter += int(1)
+
+# Создание и запуск потоков
+t = Thread(target=increment)
+t.start()
+
+t2 = Thread(target=increment)
+t2.start()
+
+# Дожидаемся завершения работы потоков
+t.join()
+t2.join()
+# Значение счетчика всегда будет разным
+print(counter)
---
+++ threading/race_condition_solved.py
@@ -0,0 +1,27 @@
+# python 3.9.5
+
+from threading import Thread, Lock
+
+counter = 0
+
+# Объект блокировки
+lock = Lock()
+
+def increment():
+    global counter
+    for _ in range(2000000):
+        # С помощью контекстного менеджера захватываем блокировку 
+        # и отпускаем, как только выходим из него
+        with lock:
+            counter += int(1)
+
+
+t = Thread(target=increment)
+t.start()
+
+t2 = Thread(target=increment)
+t2.start()
+
+t.join()
+t2.join()
+print(counter)
